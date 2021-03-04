cb3205dcaab7a1829095d073991f77a756a6001c
nikvoronin <nvoronin@outlook.com>
2021-03-04 16:47:00 +0300

    Add initial cs-project structure

---
+++ src/ToptoutCli/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
+namespace ToptoutCli
+{
+    class Program
+    {
+        static int Main( string[] args )
+        {
+            Console.WriteLine( "Toptout-cli 0.1.304-alpha" );
+
+            var cmd = new RootCommand( description: "Easily opt-out from telemetry collection." );
+            cmd.AddCommand( CreateCommand(
+                new string[] { "update", "u", "force" },
+                "Download and update local database." ,
+                () => {
+                    // TODO Download json data
+                    Console.WriteLine("[UPDATE] handler");
+                }));
+
+            cmd.Handler = CommandHandler.Create( ( ) => {
+                // TODO do toptout things
+                Console.WriteLine( "[ROOT] handler" );
+            } );
+
+            int errlevel = cmd.Invoke( args );
+
+            Console.WriteLine( "Press [Enter] to close..." );
+            Console.ReadLine();
+
+            return errlevel;
+        }
+
+        internal static Command CreateCommand( string[] aliases, string description, Action handler )
+        {
+            var cmd = new Command( 
+                name: aliases[0],
+                description: description
+                );
+
+            if (aliases != null) {
+                for(int i = 1; i < aliases.Length; i++)
+                    cmd.AddAlias( aliases[i]);
+            }
+
+            cmd.Handler = CommandHandler.Create( handler );
+
+            return cmd;
+        }
+    }
+}
