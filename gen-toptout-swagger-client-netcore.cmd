df90c123d62dec40546e86600a157ada45d36572
nikvoronin <nvoronin@outlook.com>
2021-03-05 14:18:55 +0300

    Add download openapi schemas

--- gen-toptout-swagger-client-netcore.cmd
+++ gen-toptout-swagger-client-netcore.cmd
@@ -1,7 +1,16 @@
-if not exist "./bin" mkdir "./bin"
+setlocal
+
+mkdir bin
+mkdir lib
 
 if not exist ./bin/swagger-codegen-cli.jar (
 	curl.exe --output ./bin/swagger-codegen-cli.jar --url https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.25/swagger-codegen-cli-3.0.25.jar
 )
 
+curl.exe --output openapi.yaml --url https://raw.githubusercontent.com/beatcracker/toptout/master/schema/openapi.yaml
+
+curl.exe --output toptout.schema.json --url https://raw.githubusercontent.com/beatcracker/toptout/master/schema/toptout.schema.json
+
 java -jar ./bin/swagger-codegen-cli.jar generate -i https://raw.githubusercontent.com/beatcracker/toptout/master/schema/openapi.yaml -l csharp -o ./src/ToptoutCli.SwaggerClient --additional-properties targetFramework=v5.0,netCoreProjectFile=true
+
+endlocal
