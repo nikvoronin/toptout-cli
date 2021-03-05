fe6682eb799b80df5df6bad92a2be984b6b805d5
nikvoronin <nvoronin@outlook.com>
2021-03-05 14:22:55 +0300

    Move api schemas to -schema folder. Update -gen* script

--- gen-toptout-swagger-client-netcore.cmd
+++ gen-toptout-swagger-client-netcore.cmd
@@ -2,14 +2,15 @@ setlocal
 
 mkdir bin
 mkdir lib
+mkdir schema
 
 if not exist ./bin/swagger-codegen-cli.jar (
 	curl.exe --output ./bin/swagger-codegen-cli.jar --url https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.25/swagger-codegen-cli-3.0.25.jar
 )
 
-curl.exe --output openapi.yaml --url https://raw.githubusercontent.com/beatcracker/toptout/master/schema/openapi.yaml
+curl.exe --output ./schema/openapi.yaml --url https://raw.githubusercontent.com/beatcracker/toptout/master/schema/openapi.yaml
 
-curl.exe --output toptout.schema.json --url https://raw.githubusercontent.com/beatcracker/toptout/master/schema/toptout.schema.json
+curl.exe --output ./schema/toptout.schema.json --url https://raw.githubusercontent.com/beatcracker/toptout/master/schema/toptout.schema.json
 
 java -jar ./bin/swagger-codegen-cli.jar generate -i https://raw.githubusercontent.com/beatcracker/toptout/master/schema/openapi.yaml -l csharp -o ./src/ToptoutCli.SwaggerClient --additional-properties targetFramework=v5.0,netCoreProjectFile=true
 
