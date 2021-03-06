dade3951e534522f2ed973e5650880fa3aaaccc3
nikvoronin <nvoronin@outlook.com>
2021-03-06 16:43:33 +0300

    Add test to deserialize json file

--- src/ToptoutCli.Tests/GithubProvider_Tests.cs
+++ src/ToptoutCli.Tests/GithubProvider_Tests.cs
@@ -1,9 +1,9 @@
 ﻿using IO.Swagger.Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 using ToptoutCli.Provider;
 using Xunit;
@@ -53,12 +53,14 @@ namespace ToptoutCli.Tests
             var files = await provider.ListJsonFilesAsync(url);
 
             string jsonString = await provider.GetJsonFileAsString(files[0]);
+            var t = JsonConvert.DeserializeObject<Toptout>(jsonString);
 
             Assert.NotNull(url);
 
             Assert.NotNull( files );
             Assert.True( files.Count > 0 );
             Assert.False( string.IsNullOrEmpty(jsonString) );
+            Assert.NotNull( t );
         }
     }
 }
